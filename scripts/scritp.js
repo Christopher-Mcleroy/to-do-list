b0c80585619db3eef95894c3c86fafb194801201
allenwebdev <62407430+allenwebdev@users.noreply.github.com>
2020-06-17 16:37:15 -0700

    first

---
+++ scripts/scritp.js
@@ -0,0 +1,108 @@
+const listItem = document.querySelectorAll('li');
+const deleteBtn = document.querySelectorAll('span');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+const plusBtn = document.querySelector('.fa-plus');
+onload = init;
+//******************************************************************************** */
+//*****************************Loads listeners*********************************** */
+//******************************************************************************** */
+function init(){
+    liListeners();
+    deleteListener();
+    inputListener();
+    hideInputListener();
+};
+//******************************************************************************** */
+//***************************Add button listener******************************* */
+//******************************************************************************** */
+function hideInputListener(){
+    plusBtn.addEventListener('click', function(){
+        input.classList.toggle('hide');
+    });
+};
+
+
+
+
+//******************************************************************************** */
+//***************************Delete button listener******************************* */
+//******************************************************************************** */
+function deleteListener(){
+        ul.addEventListener('click', function(e){
+            deleteLi(e);
+        });
+};
+//******************************************************************************** */
+//***************************Check LI listener**************************************** */
+//******************************************************************************** */
+function liListeners(){
+        ul.addEventListener('click', function(e){
+            checked(e);
+    });
+};
+//******************************************************************************** */
+//*****************************Text Input Listener******************************** */
+//******************************************************************************** */
+function inputListener(){
+    input.addEventListener('keypress', function(e){
+        if(e.keyCode === 13){
+            addItem();
+        };
+    });
+};
+//******************************************************************************** */
+//*************Toggles checked class when Li is clicked*************************** */
+//******************************************************************************** */
+function checked(e){
+    if(e.target.classList == 'far fa-square'){
+        // change icon
+        e.target.classList = 'far fa-check-square';
+        // strike through text
+        e.target.parentElement.classList.toggle('checked');
+    }else if(e.target.classList == 'far fa-check-square'){
+        e.target.parentElement.classList.toggle('checked');
+        e.target.classList = 'far fa-square';
+    }
+};
+//******************************************************************************** */
+//***********************Deletes Li when delete btn is pressed********************* */
+//******************************************************************************** */
+function deleteLi(e){
+    if(e.target.classList == 'fas fa-trash'){
+        e.target.parentElement.remove();
+    }
+};
+//******************************************************************************** */
+//**********************Adds li when input is submitted**************************** */
+//******************************************************************************** */
+function addItem(){
+    // create li
+    let li = document.createElement('li');
+    //make trash icon
+    let trash = document.createElement('i');
+    //add class to trash icon
+    trash.classList = 'fas fa-trash';
+    //append trash icon
+    li.appendChild(trash);
+    // create check icon
+    let check = document.createElement('i');
+    // add class to icon
+    check.classList = 'far fa-square';
+    //append icon to li
+    li.appendChild(check);
+    // create text node
+    let itemText = document.createTextNode(` ${input.value}`);
+    // create span
+    let span = document.createElement('span');
+    // add attr
+    span.setAttribute('contenteditable', 'true');
+    // append text node to span
+    span.appendChild(itemText);
+    //append span to li
+    li.appendChild(span)
+    // append to ul
+    ul.appendChild(li);
+    // clear input
+    input.value = '';
+};
