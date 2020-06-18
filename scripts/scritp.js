c05d5520444726eba1b9970b1391bd691c0509eb
allenwebdev <62407430+allenwebdev@users.noreply.github.com>
2020-06-18 11:36:19 -0700

    word over flow added

--- scripts/scritp.js
+++ scripts/scritp.js
@@ -11,6 +11,7 @@ function init(){
     liListeners();
     deleteListener();
     inputListener();
+    hideInputListener();
 };
 //******************************************************************************** */
 //***************************Add button listener******************************* */
