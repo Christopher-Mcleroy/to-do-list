b0c04a16674e7fafd724d4d3f9db3d919bee97bb
allenwebdev <62407430+allenwebdev@users.noreply.github.com>
2020-06-18 11:00:41 -0700

    done

--- scripts/scritp.js
+++ scripts/scritp.js
@@ -2,6 +2,7 @@ const listItem = document.querySelectorAll('.todo-item');
 const input = document.querySelector('input');
 const ul = document.querySelector('#list-items');
 const plusBtn = document.querySelector('.fa-plus');
+const trashBtn = document.querySelector('.fa-trash');
 onload = init;
 //******************************************************************************** */
 //*****************************Loads listeners*********************************** */
@@ -10,7 +11,6 @@ function init(){
     liListeners();
     deleteListener();
     inputListener();
-    hideInputListener();
 };
 //******************************************************************************** */
 //***************************Add button listener******************************* */
@@ -20,9 +20,6 @@ function hideInputListener(){
         input.classList.toggle('hide');
     });
 };
-
-
-
 
 //******************************************************************************** */
 //***************************Delete button listener******************************* */
@@ -77,19 +74,21 @@ function deleteLi(e){
 //******************************************************************************** */
 function addItem(){
     // create li
-    let li = document.createElement('div');
+    let div = document.createElement('div');
+    // add class to div
+    div.classList = 'todo-item';
     //make trash icon
     let trash = document.createElement('i');
     //add class to trash icon
     trash.classList = 'fas fa-trash';
     //append trash icon
-    li.appendChild(trash);
+    div.appendChild(trash);
     // create check icon
     let check = document.createElement('i');
     // add class to icon
     check.classList = 'far fa-square';
     //append icon to li
-    li.appendChild(check);
+    div.appendChild(check);
     // create text node
     let itemText = document.createTextNode(` ${input.value}`);
     // create span
@@ -99,9 +98,9 @@ function addItem(){
     // append text node to span
     span.appendChild(itemText);
     //append span to li
-    li.appendChild(span)
+    div.appendChild(span)
     // append to ul
-    ul.appendChild(li);
+    ul.appendChild(div);
     // clear input
     input.value = '';
 };
